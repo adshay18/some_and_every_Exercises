67f4b018bb82defd5b62fd27121b1999c072f01d
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2021-11-17 22:44:17 -0500

    write every funcs

--- some-every.js
+++ some-every.js
@@ -35,7 +35,9 @@ Examples:
 */
 
 function hasOnlyOddNumbers(arr) {
-   
+    return arr.every(function(num) {
+        return num % 2 !== 0;
+    })
 }
 
 /*
@@ -46,7 +48,12 @@ Examples:
     hasNoDuplicates([1,2,3]) // true
 */
 
-function hasNoDuplicates(arr) {}
+function hasNoDuplicates(arr) {
+    return arr.every(function(val) {
+        return arr.indexOf(val) === arr.lastIndexOf(val);
+    })
+}
+
 
 /*
 Write a function called hasCertainKey which accepts an array of objects and a key, and returns true if every single object in the array contains that key. Otherwise it should return false.
@@ -63,7 +70,11 @@ Examples:
     hasCertainKey(arr,'isCatOwner') // false
 */
 
-function hasCertainKey(arr, key) {}
+function hasCertainKey(arr, key) {
+    return arr.every(function(val){
+        return val[key];
+    })
+}
 
 /*
 Write a function called hasCertainValue which accepts an array of objects and a key, and a value, and returns true if every single object in the array contains that value for the specific key. Otherwise it should return false.
@@ -81,4 +92,8 @@ Examples:
     
 */
 
-function hasCertainValue(arr, key, searchValue) {}
+function hasCertainValue(arr, key, searchValue) {
+    return arr.every(function(val) {
+        return val[key] === searchValue;
+    })
+}
